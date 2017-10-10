cf4c4fd42581dbbf98bcbdcbd00ed8c8caee38e3
Marcus Egues <marcusegues@mbp-sf.local>
2017-10-10 09:06:02 +0200

    Add duty rules

--- src/reducers/declaredBasket.js
+++ src/reducers/declaredBasket.js
@@ -1,17 +1,87 @@
-import { emptyBasket } from '../constants/basket';
+import { emptyBasket, categories } from '../constants/basket';
+
+const duty = (action, quantity) => {
+  switch (action.categoryName) {
+    case categories.MEAT_AND_MEAT_PRODUCTS: {
+      if (quantity <= 1) {
+        return 0;
+      } else if (quantity <= 10) {
+        return 17 * (quantity - 1);
+      } else {
+        return 23 * quantity;
+      }
+    }
+    case categories.BUTTER_OR_CREAM: {
+      if (quantity <= 1) {
+        return 0;
+      } else {
+        return 16 * (quantity - 1);
+      }
+    }
+    case categories.OILS_FATS_MARGARINE: {
+      if (quantity <= 5) {
+        return 0;
+      } else {
+        return 2 * (quantity - 5);
+      }
+    }
+    case categories.ALCOHOL_BELOW_18: {
+      if (quantity <= 5) {
+        return 0;
+      } else {
+        return 2 * (quantity - 5);
+      }
+    }
+    case categories.ALCOHOL_ABOVE_18: {
+      if (quantity <= 1) {
+        return 0;
+      } else {
+        return 15 * (quantity - 1);
+      }
+    }
+    case categories.CIGARETTES_AND_CIGARS: {
+      if (quantity <= 250) {
+        return 0;
+      } else {
+        return 0.25 * (quantity - 250);
+      }
+    }
+    case categories.OTHER_TOBACCO: {
+      if (quantity <= 250) {
+        return 0;
+      } else {
+        return 0.1 * (quantity - 250);
+      }
+    }
+    default: {
+      return 0;
+    }
+  }
+};
+
+const basketItem = (state = {}, action) => {
+  switch (action.type) {
+    case 'CHANGE_QUANTITY_DECLARED_BASKET_ITEM': {
+      const quantity =
+        state[action.categoryName].quantity + action.quantityChange;
+      return {
+        ...state,
+        duty: duty(action, quantity),
+        quantity: Math.max(0, quantity),
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
 const declaredBasket = (state = emptyBasket, action) => {
   switch (action.type) {
     case 'CHANGE_QUANTITY_DECLARED_BASKET_ITEM': {
       return {
         ...state,
-        [action.categoryName]: {
-          ...state[action.categoryName],
-          quantity: Math.max(
-            0,
-            state[action.categoryName].quantity + action.quantityChange
-          ),
-        },
+        [action.categoryName]: basketItem(state[action.categoryName], action),
       };
     }
     default: {
