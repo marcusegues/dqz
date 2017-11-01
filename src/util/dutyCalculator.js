e37afb53487a79f3e8b767fd8740bb9d49189ba7
Marcus Egues <marcusegues@mbp-sf.home>
2017-11-01 14:30:11 +0100

    duty calculator

---
+++ src/util/dutyCalculator.js
@@ -0,0 +1,96 @@
+import {
+  dutyFreeAllowances,
+  MEAT_AND_MEAT_PRODUCTS_FIRST_DUTY_THRESHOLD,
+} from './duty';
+export const dutyBySubCategory = (
+  subCategoryName,
+  quantity,
+  numberPersonsOver17,
+  numberPersonsUnder17
+) => {
+  const totalPersons = numberPersonsUnder17 + numberPersonsOver17;
+  switch (subCategoryName) {
+    case subCategories.MEAT_AND_MEAT_PRODUCTS: {
+      const dutyFreeAllowance =
+        totalPersons * dutyFreeAllowances.MEAT_AND_MEAT_PRODUCTS;
+      const threshold =
+        numberPersons * MEAT_AND_MEAT_PRODUCTS_FIRST_DUTY_THRESHOLD;
+      if (quantity <= dutyFreeAllowance) {
+        return 0;
+      } else if (quantity <= threshold) {
+        return (
+          MEAT_AND_MEAT_PRODUCTS_DUTY_UNDER_THRESHOLD *
+          (quantity - dutyFreeAllowance)
+        );
+      } else {
+        return (
+          MEAT_AND_MEAT_PRODUCTS_DUTY_UNDER_THRESHOLD *
+            (threshold - dutyFreeAllowance) +
+          MEAT_AND_MEAT_PRODUCTS_DUTY_OVER_THRESHOLD * (quantity - threshold)
+        );
+      }
+    }
+    case subCategories.BUTTER_OR_CREAM: {
+      const dutyFreeAllowance =
+        totalPersons * dutyFreeAllowances.BUTTER_OR_CREAM;
+      if (quantity <= dutyFreeAllowance) {
+        return 0;
+      } else {
+        return BUTTER_OR_CREAM_DUTY * (quantity - dutyFreeAllowance);
+      }
+    }
+    case subCategories.OILS_FATS_MARGARINE: {
+      const dutyFreeAllowance =
+        totalPersons * dutyFreeAllowances.OILS_FATS_MARGARINE;
+      if (quantity <= dutyFreeAllowance) {
+        return 0;
+      } else {
+        return OILS_FATS_MARGARINE_DUTY * (quantity - dutyFreeAllowance);
+      }
+    }
+    case subCategories.ALCOHOL_BELOW_18: {
+      const dutyFreeAllowance =
+        totalPersonsUnder17 * dutyFreeAllowances.ALCOHOL_BELOW_18;
+      if (quantity <= dutyFreeAllowance) {
+        return 0;
+      } else {
+        return ALCOHOL_BELOW_18_DUTY * (quantity - dutyFreeAllowance);
+      }
+    }
+    case subCategories.ALCOHOL_ABOVE_18: {
+      const dutyFreeAllowance =
+        totalPersonsUnder17 * dutyFreeAllowances.ALCOHOL_ABOVE_18;
+      if (quantity <= dutyFreeAllowance) {
+        return 0;
+      } else {
+        return ALCOHOL_ABOVE_18_DUTY * (quantity - dutyFreeAllowance);
+      }
+    }
+    case subCategories.CIGARETTES_AND_CIGARS: {
+      const dutyFreeAllowance =
+        totalPersons * dutyFreeAllowances.CIGARETTES_AND_CIGARS;
+      if (quantity <= dutyFreeAllowance) {
+        return 0;
+      } else {
+        return CIGARETTES_AND_CIGARS_DUTY * (quantity - dutyFreeAllowance);
+      }
+    }
+    case subCategories.OTHER_TOBACCO: {
+      const dutyFreeAllowance = totalPersons * dutyFreeAllowances.OTHER_TOBACCO;
+      if (quantity <= dutyFreeAllowance) {
+        return 0;
+      } else {
+        return OTHER_TOBACCO_DUTY * (quantity - dutyFreeAllowance);
+      }
+    }
+    default: {
+      return 0;
+    }
+  }
+};
+
+const dutyCalculator = (
+  basket,
+  numberPersonsOver17,
+  numberPersonsUnder17
+) => {};
