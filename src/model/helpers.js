20ccf317a113d3d3b8f6ddfd3873a83459b9d3f5
Christian Roth <chroth@me.com>
2017-11-02 22:36:05 +0100

    Safety hatch.

--- src/model/helpers.js
+++ src/model/helpers.js
@@ -7,9 +7,9 @@ const emptyItem = {
   dues: { duty: 0, vat: 0 },
 };
 
-// todo make sure the objects are deep copied (I want immutable...)
+// TODO make sure the objects are deep copied (I want immutable...) (added Object.assign for good measure)
 export const emptyBasket: BasketType = new Map(
-  CategoriesArray.map(c => [c, emptyItem])
+  CategoriesArray.map(c => [c, Object.assign({}, emptyItem)])
 );
 
 export const initPeople: People = {
