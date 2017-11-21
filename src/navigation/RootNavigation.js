3a0068a1f4f7886acd3c005b2c0116e4a96409d2
Marcus Egues <marcusegues@mbp-sf.home>
2017-11-21 15:56:47 +0100

    Remove tab prettier pref

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -7,9 +7,9 @@ import OnBoarding from '../screens/OnBoarding/OnBoarding';
 import * as colors from '../styles/colors';
 
 import {
-	HeaderTitle,
-	HeaderLeft,
-	HeaderRight,
+  HeaderTitle,
+  HeaderLeft,
+  HeaderRight,
 } from '../components/headers/AppHeader';
 
 import MainMenu from '../screens/MainMenu/MainMenu';
@@ -17,71 +17,71 @@ import MainMenu from '../screens/MainMenu/MainMenu';
 import registerForPushNotificationsAsync from '../../api/registerForPushNotificationsAsync';
 
 const RootStackNavigator = StackNavigator(
-	{
-		QuestionAnswer: {
-			screen: QuestionAnswerFlow,
-			navigationOptions: () => ({
-				title: `Waren deklarieren`,
-			}),
-		},
-		OnBoarding: {
-			screen: MainMenu,
-		},
-		PeopleInput: {
-			screen: PeopleInputContainer,
-		},
-	},
-	{
-		navigationOptions: navigation => ({
-			// header: null,
-			headerLeft: <HeaderLeft />,
-			headerRight: <HeaderRight />,
-			headerTitle: <HeaderTitle />,
+  {
+    QuestionAnswer: {
+      screen: QuestionAnswerFlow,
+      navigationOptions: () => ({
+        title: `Waren deklarieren`,
+      }),
+    },
+    OnBoarding: {
+      screen: MainMenu,
+    },
+    PeopleInput: {
+      screen: PeopleInputContainer,
+    },
+  },
+  {
+    navigationOptions: navigation => ({
+      // header: null,
+      headerLeft: <HeaderLeft />,
+      headerRight: <HeaderRight />,
+      headerTitle: <HeaderTitle />,
 
-			headerStyle: {
-				paddingRight: 15,
-				paddingLeft: 15,
-				borderBottomWidth: 5,
-				borderBottomColor: '#EA0000',
-			},
-			headerTitleStyle: {},
-		}),
-		cardStyle: { backgroundColor: colors.MAIN_BACKGROUND_COLOR },
-	},
-	{
-		initialRouteName: 'OnBoarding',
-	}
+      headerStyle: {
+        paddingRight: 15,
+        paddingLeft: 15,
+        borderBottomWidth: 5,
+        borderBottomColor: '#EA0000',
+      },
+      headerTitleStyle: {},
+    }),
+    cardStyle: { backgroundColor: colors.MAIN_BACKGROUND_COLOR },
+  },
+  {
+    initialRouteName: 'OnBoarding',
+  }
 );
 
 export default class RootNavigator extends React.Component {
-	componentDidMount() {
-		this._notificationSubscription = this._registerForPushNotifications();
-	}
+  componentDidMount() {
+    this._notificationSubscription = this._registerForPushNotifications();
+  }
 
-	componentWillUnmount() {
-		this._notificationSubscription && this._notificationSubscription.remove();
-	}
+  componentWillUnmount() {
+    this._notificationSubscription && this._notificationSubscription.remove();
+  }
 
-	render() {
-		return <RootStackNavigator />;
-	}
+  render() {
+    return <RootStackNavigator />;
+  }
 
-	_registerForPushNotifications() {
-		// Send our push token over to our backend so we can receive notifications
-		// You can comment the following line out if you want to stop receiving
-		// a notification every time you open the app. Check out the source
-		// for this function in api/registerForPushNotificationsAsync.js
-		registerForPushNotificationsAsync();
+  _registerForPushNotifications() {
+    // Send our push token over to our backend so we can receive notifications
+    // You can comment the following line out if you want to stop receiving
+    // a notification every time you open the app. Check out the source
+    // for this function in api/registerForPushNotificationsAsync.js
+    registerForPushNotificationsAsync();
 
-		// Watch for incoming notifications
-		this._notificationSubscription = Notifications.addListener(
-			this._handleNotification
-		);
-	}
+    // Watch for incoming notifications
+    this._notificationSubscription = Notifications.addListener(
+      this._handleNotification
+    );
+  }
 
-	_handleNotification = ({ origin, data }) => {
-		console.log(
-			`Push notification ${origin} with data: ${JSON.stringify(data)}`
-		);
-	};
+  _handleNotification = ({ origin, data }) => {
+    console.log(
+      `Push notification ${origin} with data: ${JSON.stringify(data)}`
+    );
+  };
 }
