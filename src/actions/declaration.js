80a6b99109f4743b387a8f3249afdcacc7b7bf6e
Christian Roth <chroth@me.com>
2017-11-28 20:16:31 +0100

    ESLint on two files.

--- src/actions/declaration.js
+++ src/actions/declaration.js
@@ -1,12 +1,10 @@
-export const toggleMainCategory = mainCategory => {
-  return (dispatch, getState) => {
-    const state = getState();
-    if (
-      state.declaration.getIn(['settings', 'mainCategories']).has(mainCategory)
-    ) {
-      dispatch({ type: 'DECLARATION_REMOVE_MAIN_CATEGORY', mainCategory });
-    } else {
-      dispatch({ type: 'DECLARATION_ADD_MAIN_CATEGORY', mainCategory });
-    }
-  };
+export const toggleMainCategory = mainCategory => (dispatch, getState) => {
+  const state = getState();
+  if (
+    state.declaration.getIn(['settings', 'mainCategories']).has(mainCategory)
+  ) {
+    dispatch({ type: 'DECLARATION_REMOVE_MAIN_CATEGORY', mainCategory });
+  } else {
+    dispatch({ type: 'DECLARATION_ADD_MAIN_CATEGORY', mainCategory });
+  }
 };
--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -1,41 +1,36 @@
 import React from 'react';
 import { View } from 'react-native';
-import { MAIN_RED } from '../../styles/colors';
 import Touchable from 'react-native-platform-touchable';
+import { MAIN_RED } from '../../styles/colors';
 
-class CheckBox extends React.Component {
-  render() {
-    const { checked } = this.props;
-
-    return (
-      <Touchable
+const CheckBox = ({ checked, onPress }) => (
+  <Touchable
+    style={{
+      height: 17,
+      width: 17,
+      borderRadius: 8.5,
+      borderColor: 'black',
+      borderWidth: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+    onPress={onPress}
+  >
+    {checked ? (
+      <View
+        checked={checked}
         style={{
-          height: 17,
-          width: 17,
-          borderRadius: 8.5,
-          borderColor: 'black',
-          borderWidth: 1,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
+          height: 12,
+          width: 12,
+          borderRadius: 6,
+          backgroundColor: MAIN_RED,
         }}
-        onPress={this.props.onPress}
-      >
-        {checked ? (
-          <View
-            checked={checked}
-            style={{
-              height: 12,
-              width: 12,
-              borderRadius: 6,
-              backgroundColor: MAIN_RED,
-            }}
-          />
-        ) : (
-          <View />
-        )}
-      </Touchable>
-    );
-  }
-}
+      />
+    ) : (
+      <View />
+    )}
+  </Touchable>
+);
+
 export default CheckBox;
