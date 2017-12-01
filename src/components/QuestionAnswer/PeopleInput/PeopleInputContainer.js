16951b635f378286d1bd28150e0d70d62f1ba92c
Marcus Egues <marcusegues@mbp-sf.local>
2017-12-01 10:07:33 +0100

    Implement components

---
+++ src/components/QuestionAnswer/PeopleInput/PeopleInputContainer.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { View } from 'react-native';
+import PeopleInput from './PeopleInput';
+import { getDeclarationPeople } from '../../../reducers';
+import * as fromModelApi from '../../../model/configurationApi';
+
+class PeopleInputContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: this.props.people,
+    };
+    this.handleAddAdult = this.handleAddAdult.bind(this);
+    this.handleSubtractAdult = this.handleSubtractAdult.bind(this);
+    this.handleAddMinor = this.handleAddMinor.bind(this);
+    this.handleSubtractMinor = this.handleSubtractMinor.bind(this);
+    this.handleAnswerConfirm = this.handleAnswerConfirm.bind(this);
+    this.handleAnswer = this.handleAnswer.bind(this);
+    this.getQuestionComponent = this.getQuestionComponent.bind(this);
+    this.getAnswerComponent = this.getAnswerComponent.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { people } = nextProps;
+    if (people !== this.props.people) {
+      const adultsSaved = fromModelApi.getAdultPeople(people);
+      const minorsSaved = fromModelApi.getMinorPeople(people);
+      this.handleAnswer(adultsSaved + minorsSaved);
+    }
+  }
+
+  handleAddAdult() {
+    this.setState({
+      people: fromModelApi.addAdult(this.state.people),
+    });
+  }
+
+  handleSubtractAdult() {
+    this.setState({
+      people: fromModelApi.subtractAdult(this.state.people),
+    });
+  }
+
+  handleAddMinor() {
+    this.setState({
+      people: fromModelApi.addMinor(this.state.people),
+    });
+  }
+
+  handleSubtractMinor() {
+    this.setState({
+      people: fromModelApi.subtractMinor(this.state.people),
+    });
+  }
+
+  handleAnswerConfirm() {
+    // the logic here to manage handling the answer should be looked at again
+    // to make sure it is ideal. This is a piece of code with side effects.
+    const { onAdultsSetQuantity, onMinorsSetQuantity, people } = this.props;
+    const adults = fromModelApi.getAdultPeople(this.state.people);
+    const minors = fromModelApi.getMinorPeople(this.state.people);
+    const adultsSaved = fromModelApi.getAdultPeople(people);
+    const minorsSaved = fromModelApi.getMinorPeople(people);
+
+    if (adults === adultsSaved && minors === minorsSaved) {
+      this.handleAnswer(adults + minors);
+    } else {
+      onAdultsSetQuantity(adults);
+      onMinorsSetQuantity(minors);
+    }
+  }
+
+  handleAnswer(quantityPeople) {
+    const { onAnswer } = this.props;
+    if (quantityPeople > 1) {
+      onAnswer('confirmMultiplePersons');
+    } else {
+      onAnswer('confirmSinglePerson');
+    }
+  }
+
+  getQuestionComponent() {
+    return (
+      <PeopleInput
+        people={this.state.people}
+        onAnswerConfirm={this.handleAnswerConfirm}
+        onAddAdult={this.handleAddAdult}
+        onSubtractAdult={this.handleSubtractAdult}
+        onAddMinor={this.handleAddMinor}
+        onSubtractMinor={this.handleSubtractMinor}
+        text={this.props.text}
+      />
+    );
+  }
+
+  getAnswerComponent() {
+    return null;
+  }
+
+  render() {
+    console.log(this.props.current);
+    return (
+      <View>
+        {this.props.current === 1
+          ? this.getQuestionComponent()
+          : this.getAnswerComponent()}
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  people: getDeclarationPeople(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  onAdultsSetQuantity: quantity =>
+    dispatch({ type: 'DECLARATION_ADULTS_SET_QUANTITY', quantity }),
+  onMinorsSetQuantity: quantity =>
+    dispatch({ type: 'DECLARATION_MINORS_SET_QUANTITY', quantity }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  PeopleInputContainer,
+);
--- src/components/QuestionAnswer/QuestionAnswerContainer.js
+++ src/components/QuestionAnswer/QuestionAnswerContainer.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { View } from 'react-native';
-import PeopleInputContainer from './cards/ConfirmationCard/PeopleInput/PeopleInputContainer';
-import MoreThanOne from './MoreThanOne';
-import OnlyOne from './OnlyOne';
+import PeopleInputContainer from './PeopleInput/PeopleInputContainer';
+import LargeAmountPresentContainer from './LargeAmountPresent/LargeAmountPresentContainer';
+import OverAllowanceContainer from './OverAllowance/OverAllowanceContainer';
+import MainCategoriesContainer from './MainCategories/MainCategoriesContainer';
 import { getDeclarationPeople } from '../../reducers';
 import { getTotalPeople } from '../../model/configurationApi';
 
 class QuestionAnswerContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 1,
+    };
+  }
+
+  setCurrent(number) {
+    this.setState({
+      current: number,
+    });
+  }
+
   render() {
     return (
       <View>
-        <PeopleInputContainer />
-        {getTotalPeople(this.props.people) > 1 ? <MoreThanOne /> : <OnlyOne />}
+        <PeopleInputContainer
+          current={this.state.current}
+          onPress={() => this.setCurrent(1)}
+          onAnswerConfirm={() => this.setCurrent(2)}
+        />
       </View>
     );
   }
 }
 
+// <LargeAmountPresentContainer onPress={() => this.setCurrent(2)} />
+// <OverAllowanceContainer onPress={() => this.setCurrent(3)} />
+// <MainCategoriesContainer onPress={() => this.setCurrent(4)} />
 const mapStateToProps = state => ({
   people: getDeclarationPeople(state),
 });
