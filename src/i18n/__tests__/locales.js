9a81205b0658192ce535da8ea6e6f40d53141788
Marcus Egues <marcusegues@mbp-sf.home>
2017-12-13 11:31:19 +0100

    Fix test

--- src/i18n/__tests__/locales.js
+++ src/i18n/__tests__/locales.js
@@ -1,5 +1,8 @@
 /* global expect, test, describe */
-import { en1, en2, de1, fr1, it1 } from './exampleLocales';
+import { en } from '../locales/en';
+import { de } from '../locales/de';
+import { fr } from '../locales/fr';
+import { it } from '../locales/it';
 
 const objectsHaveSameKeys = (...objects) => {
   const allKeys = objects.reduce(
@@ -9,19 +12,6 @@ const objectsHaveSameKeys = (...objects) => {
   const union = new Set(allKeys);
   return objects.every(object => union.size === Object.keys(object).length);
 };
-
-function arrayifyKeys(object) {
-  const array = [];
-  if (typeof object === 'object' && !Array.isArray(object)) {
-    Object.keys(object).forEach(key => {
-      array.push(key);
-      if (typeof object[key] !== 'string') {
-        array.push(arrayifyKeys(object[key]));
-      }
-    });
-  }
-  return array;
-}
 
 function keyify(obj, prefix = '') {
   return Object.keys(obj).reduce((res, el) => {
@@ -34,45 +24,12 @@ function keyify(obj, prefix = '') {
   }, []);
 }
 
-describe('locales examples', () => {
-  test('1 through arrayifyKeys function generate same output', () => {
-    expect(arrayifyKeys(en1.toJS())).toEqual([
-      'onBoarding',
-      ['onBoardingMessage', 'selectLanguage'],
-      'general',
-      ['declareGoods', 'pleaseSelect'],
-      'largeAmountInput',
-      ['pleaseInput', 'totalLargeAmount'],
-    ]);
-  });
-
-  test('1 through keyify function generate same output', () => {
-    const enKeyified = keyify(en1.toJS());
-    expect(enKeyified).toEqual([
-      'onBoarding.onBoardingMessage',
-      'onBoarding.selectLanguage',
-      'general.declareGoods',
-      'general.pleaseSelect',
-      'largeAmountInput.pleaseInput',
-      'largeAmountInput.totalLargeAmount',
-    ]);
-  });
-
-  test('1 all have same keys', () => {
-    const enKeyified = keyify(en1.toJS());
-    const deKeyified = keyify(de1.toJS());
-    const frKeyified = keyify(fr1.toJS());
-    const itKeyified = keyify(it1.toJS());
-    expect(
-      objectsHaveSameKeys(enKeyified, deKeyified, frKeyified, itKeyified)
-    ).toBe(true);
-  });
-
-  test('have the untyped extra keys ignored', () => {
-    const enKeyified = keyify(en2.toJS());
-    const deKeyified = keyify(de1.toJS());
-    const frKeyified = keyify(fr1.toJS());
-    const itKeyified = keyify(it1.toJS());
+describe('locales', () => {
+  test('all have same keys', () => {
+    const enKeyified = keyify(en.toJS());
+    const deKeyified = keyify(de.toJS());
+    const frKeyified = keyify(fr.toJS());
+    const itKeyified = keyify(it.toJS());
     expect(
       objectsHaveSameKeys(enKeyified, deKeyified, frKeyified, itKeyified)
     ).toBe(true);
