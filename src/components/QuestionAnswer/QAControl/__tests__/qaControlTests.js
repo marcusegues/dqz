42de03212051378e6fd0c5c4d66f138f67d05df1
Christian Roth <chroth@me.com>
2017-12-16 22:48:19 +0100

    Add TCs for setQuestionStates and collapseAllExistingExceptOne.

--- src/components/QuestionAnswer/QAControl/__tests__/qaControlTests.js
+++ src/components/QuestionAnswer/QAControl/__tests__/qaControlTests.js
@@ -1,6 +1,10 @@
 /* global expect, test, describe */
 import Immutable from 'immutable';
-import { setInitStates } from '../controlQuestionStates';
+import {
+  collapseAllExistingExceptOne,
+  setInitStates,
+  setQuestionStates,
+} from '../controlQuestionStates';
 import { emptyBasket, initPeople } from '../../../../model/configurationApi';
 import { makeSettingsRecord } from '../../../../types/reducers/declaration';
 
@@ -20,9 +24,9 @@ const stateWithMainCategories = {
   people: initPeople,
   settings: makeSettingsRecord().set('mainCategories', Immutable.Set('Foo')),
   questionStates: {
-    peopleInput: 'expanded',
-    mainCategories: 'hidden',
-    quantityInput: 'hidden',
+    peopleInput: 'collapsed',
+    mainCategories: 'collapsed',
+    quantityInput: 'collapsed',
   },
 };
 
@@ -39,5 +43,77 @@ describe('Test qa control flow', () => {
     expect(newState.peopleInput).toBe('collapsed');
     expect(newState.mainCategories).toBe('collapsed');
     expect(newState.quantityInput).toBe('collapsed');
+  });
+
+  test('after people input on blank state', () => {
+    const newState = setQuestionStates('peopleInput', blankState)
+      .questionStates;
+    expect(newState.peopleInput).toBe('collapsed');
+    expect(newState.mainCategories).toBe('expanded');
+    expect(newState.quantityInput).toBe('hidden');
+  });
+
+  test('after people input with main categories already present', () => {
+    const newState = setQuestionStates('peopleInput', stateWithMainCategories)
+      .questionStates;
+    expect(newState.peopleInput).toBe('collapsed');
+    expect(newState.mainCategories).toBe('collapsed');
+    expect(newState.quantityInput).toBe('collapsed');
+  });
+
+  test('after main categories', () => {
+    const newState = setQuestionStates(
+      'mainCategories',
+      stateWithMainCategories
+    ).questionStates;
+    expect(newState.peopleInput).toBe('collapsed');
+    expect(newState.mainCategories).toBe('collapsed');
+    expect(newState.quantityInput).toBe('expanded');
+  });
+
+  test('after quantity input', () => {
+    const newState = setQuestionStates('quantityInput', stateWithMainCategories)
+      .questionStates;
+    expect(newState.peopleInput).toBe('collapsed');
+    expect(newState.mainCategories).toBe('collapsed');
+    expect(newState.quantityInput).toBe('collapsed');
+  });
+
+  test('collapsing all but people input on blank', () => {
+    const newState = collapseAllExistingExceptOne('peopleInput', blankState)
+      .questionStates;
+    expect(newState.peopleInput).toBe('expanded');
+    expect(newState.mainCategories).toBe('hidden');
+    expect(newState.quantityInput).toBe('hidden');
+  });
+
+  test('collapsing all but people input with main categories', () => {
+    const newState = collapseAllExistingExceptOne(
+      'peopleInput',
+      stateWithMainCategories
+    ).questionStates;
+    expect(newState.peopleInput).toBe('expanded');
+    expect(newState.mainCategories).toBe('collapsed');
+    expect(newState.quantityInput).toBe('collapsed');
+  });
+
+  test('collapsing all but main categories', () => {
+    const newState = collapseAllExistingExceptOne(
+      'mainCategories',
+      stateWithMainCategories
+    ).questionStates;
+    expect(newState.peopleInput).toBe('collapsed');
+    expect(newState.mainCategories).toBe('expanded');
+    expect(newState.quantityInput).toBe('collapsed');
+  });
+
+  test('collapsing all but quantity input', () => {
+    const newState = collapseAllExistingExceptOne(
+      'quantityInput',
+      stateWithMainCategories
+    ).questionStates;
+    expect(newState.peopleInput).toBe('collapsed');
+    expect(newState.mainCategories).toBe('collapsed');
+    expect(newState.quantityInput).toBe('expanded');
   });
 });
