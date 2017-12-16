46db7253cfc9d073c7c62f1c7768e6df6a597ffe
Christian Roth <chroth@me.com>
2017-12-16 23:20:17 +0100

    Flowtype and rewrite QuantityInputQA.js.

--- src/components/QuestionAnswer/QuantityInput/QuantityInputQA.js
+++ src/components/QuestionAnswer/QuantityInput/QuantityInputQA.js
@@ -1,80 +1,67 @@
+// @flow
 import React from 'react';
+// $FlowFixMe
 import { View } from 'react-native';
 import AnswerCard from '../cards/AnswerCard/AnswerCard';
 import QuantityInputConfirmationCard from '../cards/ConfirmationCard/configured/QuantityInput/QuantityInputConfirmationCard';
 import { setQuantity, getQuantity } from '../../../model/configurationApi';
 import { MainCategoriesToCategories } from '../../../types/reducers/declaration';
+import type { Category } from '../../../model/types/basketPeopleTypes';
 
 const complete = require('../../../../assets/images/complete.png');
 const incomplete = require('../../../../assets/images/incomplete.png');
 const mainIcon = require('../../../../assets/icons/mainCategories.png');
 
-class QuantityInputContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getQuestionComponent = this.getQuestionComponent.bind(this);
-    this.getAnswerComponent = this.getAnswerComponent.bind(this);
-    this.getDisplayedCategoriesByMainCategory = this.getDisplayedCategoriesByMainCategory.bind(
-      this
-    );
-    this.handleUpdate = this.handleUpdate.bind(this);
-  }
-
-  getDisplayedCategoriesByMainCategory() {
-    return MainCategoriesToCategories.filter((cats, mainCat) =>
-      this.props.qaState.settings.get('mainCategories').has(mainCat)
-    );
-  }
-
-  getQuestionComponent() {
-    return (
-      <QuantityInputConfirmationCard
-        onAnswer={this.props.onAnswer}
-        categoriesByMainCategory={this.getDisplayedCategoriesByMainCategory()}
-        basket={this.props.qaState.basket}
-        onBasketChangeQuantity={this.handleUpdate}
-      />
-    );
-  }
-
-  getAnswerComponent() {
-    return (
-      <AnswerCard
-        onAnswerCardPress={this.props.onAnswerCardPress}
-        mainIcon={mainIcon}
-        status={
-          this.props.qaState.settings.get('mainCategories').isEmpty()
-            ? incomplete
-            : complete
-        }
-      />
-    );
-  }
-
-  handleUpdate(category, quantityChange) {
-    const { basket } = this.props.qaState;
+// todo: flowtype props
+const QuantityInputContainer = (props: any) => {
+  const handleUpdate = (category: Category, quantityChange: number) => {
+    const { basket } = props.qaState;
 
     const updatedBasket = setQuantity(
       basket,
       category,
       Math.max(0, getQuantity(basket, category) + quantityChange)
     );
-    return this.props.onUpdate(updatedBasket);
-  }
+    return props.onUpdate(updatedBasket);
+  };
 
-  render() {
-    switch (this.props.questionState) {
-      case 'expanded': {
-        return this.getQuestionComponent();
+  const getDisplayedCategoriesByMainCategory = () =>
+    MainCategoriesToCategories.filter((cats, mainCat) =>
+      props.qaState.settings.get('mainCategories').has(mainCat)
+    );
+
+  const getQuestionComponent = () => (
+    <QuantityInputConfirmationCard
+      onAnswer={props.onAnswer}
+      categoriesByMainCategory={getDisplayedCategoriesByMainCategory()}
+      basket={props.qaState.basket}
+      onBasketChangeQuantity={handleUpdate}
+    />
+  );
+
+  const getAnswerComponent = () => (
+    <AnswerCard
+      onAnswerCardPress={props.onAnswerCardPress}
+      mainIcon={mainIcon}
+      status={
+        props.qaState.settings.get('mainCategories').isEmpty()
+          ? incomplete
+          : complete
       }
-      case 'collapsed': {
-        return this.getAnswerComponent();
-      }
-      default: {
-        return <View />;
-      }
+    />
+  );
+
+  switch (props.questionState) {
+    case 'expanded': {
+      return getQuestionComponent();
+    }
+    case 'collapsed': {
+      return getAnswerComponent();
+    }
+    default: {
+      return <View />;
     }
   }
-}
+};
 
 export default QuantityInputContainer;
