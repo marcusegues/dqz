f09f38c436a793883df7c919c6548403a308d090
Marcus Egues <marcusegues@mbp-sf.home>
2017-12-20 13:20:46 +0100

    Remove unused styles

--- src/components/QuestionAnswer/cards/ConfirmationCard/ConfirmationCard.js
+++ src/components/QuestionAnswer/cards/ConfirmationCard/ConfirmationCard.js
@@ -7,35 +7,6 @@ import CardHeader from '../subcomponents/CardHeader';
 import { BLACK } from '../../../../styles/colors';
 
 const ownStyles = {
-  contentContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginTop: verticalScale(5),
-    marginBottom: verticalScale(5),
-    backgroundColor: '#fff',
-    width: '100%',
-    borderRadius: 2,
-    ...Platform.select({
-      ios: {
-        shadowColor: BLACK,
-        shadowOffset: { height: 1, width: 1 },
-        shadowOpacity: 0.25,
-        shadowRadius: 2,
-      },
-      android: {
-        elevation: 2,
-      },
-    }),
-  },
-  mainText: {
-    fontFamily: 'roboto_regular',
-    fontSize: moderateScale(24),
-    lineHeight: verticalScale(32),
-    paddingVertical: verticalScale(15),
-    color: '#141414',
-  },
   bottomSectionWrapper: {
     alignItems: 'center',
     flex: 1,
