96fef204d6c0ad413e70cd6af8e4a1ed07274b65
Christian Roth <chroth@me.com>
2017-12-23 14:35:10 +0100

    Refactor onboarding screen with flow and proper state.

--- src/screens/OnBoarding/OnBoarding.js
+++ src/screens/OnBoarding/OnBoarding.js
@@ -1,4 +1,6 @@
+// @flow
 import React from 'react';
+// $FlowFixMe
 import { View } from 'react-native';
 import { translate } from 'react-i18next';
 import { LanguageButton } from './subcomponents/LanguageButton';
@@ -7,29 +9,41 @@ import styles from './styles/onBoarding';
 import OnBoardingContainer from './subcomponents/OnBoardingContainer';
 import OnBoardingParagraph from './subcomponents/OnBoardingParagraph';
 import DoneButton from './subcomponents/DoneButton';
+import type { LanguageCodeType } from './types';
 
-class OnBoarding extends React.Component {
+type OnBoardingState = {
+  systemLanguage: LanguageCodeType,
+};
+
+type OnBoardingProps = {
+  t: (field: string, params?: {}) => void,
+  i18n: any,
+  navigation: any,
+};
+
+class OnBoarding extends React.Component<OnBoardingProps, OnBoardingState> {
   constructor(props) {
     super(props);
-    this.systemLanguage = this.props.i18n.language;
+    this.state = { systemLanguage: this.props.i18n.language };
   }
   render() {
     const { t, i18n, navigation } = this.props;
+    const { systemLanguage } = this.state;
     return (
       <OnBoardingContainer>
         <OnBoardingParagraph text={t('onBoarding:onBoardingMessage')} />
         <LanguageButton
           size="large"
-          selected={this.systemLanguage === i18n.language}
-          languageCode={this.systemLanguage}
-          onPress={() => i18n.changeLanguage(this.systemLanguage)}
+          selected={systemLanguage === i18n.language}
+          languageCode={systemLanguage}
+          onPress={() => i18n.changeLanguage(systemLanguage)}
         />
 
         <View style={styles.languageButtonsSection}>
           <OnBoardingParagraph text={t('onBoarding:selectLanguage')} />
           <View style={styles.languageButtons}>
             {languageCodeList.map(language => {
-              if (language === this.systemLanguage) {
+              if (language === systemLanguage) {
                 return null;
               }
               return (
