67b87e98f86ecd502fa12b617cad8ec4ed73ed0f
Jevgenijs Brencis <jevgen@mail.ru>
2017-12-28 12:53:10 +0200

    --CurrencyPicker created

--- src/components/Modals/CurrencyPickerModal/CurrencyPickerModal.js
+++ src/components/Modals/CurrencyPickerModal/CurrencyPickerModal.js
@@ -6,82 +6,27 @@ import { translate } from 'react-i18next';
 import AppModal from '../AppModal';
 import RedButton from '../../Buttons/RedButton';
 import ownStyles from '../styles/PickerModal';
-import { CategoriesInfo } from '../../../model/constants';
 import PickerCard from './subComponents/PickerCard';
 import PickerComponent from './subComponents/PickerComponent';
-import PickerUnits from './subComponents/PickerUnits';
 import CardHeader from '../../QuestionAnswer/cards/subcomponents/CardHeader';
 import CardHeaderSubText from '../../QuestionAnswer/cards/subcomponents/CardHeaderSubText';
 import { currencyPicker } from './currencyPickerData';
 import { moderateScale } from '../../../styles/Scaling';
 
 type PickerState = {
-  selected: 'standardInput' | 'customInput',
-  standardInput: {
-    number: string,
-    amount: string,
-  },
-  customInput: {
-    units: string,
-    decimalUnits: string,
-  },
   currency: string,
   amount: number,
 };
 
 class CurrencyPickerModal extends React.Component<any, PickerState> {
   state = {
-    selected: 'standardInput' || 'customInput',
-    standardInput: {
-      number: '1',
-      amount: '1',
-    },
-    customInput: {
-      units: '1',
-      decimalUnits: '00',
-    },
     amount: '',
     currency: 'EUR',
   };
 
-  standardTotalAmount(): number {
-    const {
-      number: numberString,
-      amount: amountString,
-    } = this.state.standardInput;
-
-    const number = +numberString;
-    const amount = +amountString;
-    if (number < 0 || amount < 0) {
-      return 0;
-    }
-    return number * amount;
-  }
-
-  customTotalAmount(): number {
-    const {
-      units: unitsString,
-      decimalUnits: decimalUnitsString,
-    } = this.state.customInput;
-
-    const units = +unitsString;
-    const decimalUnits = +decimalUnitsString;
-    if (units < 0 || decimalUnits < 0) {
-      return 0;
-    }
-    return units + decimalUnits / 100;
-  }
-
   render() {
-    const { selected } = this.state;
-    const { t, confirmAction, category } = this.props;
-    const standardInput = selected === 'standardInput';
-    const unit = CategoriesInfo.getIn([category, 'unit'], '');
-
-    const currentAmount: number = standardInput
-      ? this.standardTotalAmount()
-      : this.customTotalAmount();
-
+    const { t } = this.props;
+    const { amount, currency } = this.state;
     return (
       <AppModal
         onRequestClose={this.props.onRequestClose}
@@ -105,9 +50,10 @@ class CurrencyPickerModal extends React.Component<any, PickerState> {
                 fontSize: moderateScale(24),
                 color: '#1a1a1a',
               }}
-              onChangeText={amount => this.setState({ amount })}
+              onChangeText={value => this.setState({ amount: value })}
               value={this.state.amount}
               autoFocus
+              numeric
             />
 
             <PickerComponent
@@ -127,22 +73,19 @@ class CurrencyPickerModal extends React.Component<any, PickerState> {
                 <Picker.Item key={i.label} label={i.label} value={i.value} />
               ))}
             </PickerComponent>
-            <PickerUnits unit={unit} />
           </View>
 
           <View style={[ownStyles.redButtonWrapper, {}]}>
             <RedButton
-              onPress={() => confirmAction(currentAmount)}
+              onPress={() => {}}
               text={t(['confirmPicker'], {
-                value: `${currentAmount.toFixed(2)} ${unit}`,
+                value: `${+amount} ${currency}`,
               })}
             />
           </View>
           <CardHeaderSubText
             style={{ marginTop: 10, textAlign: 'center' }}
-            text={`${this.state.amount} ${
-              this.state.currency
-            } (Kurs vom 11.12.2017)`}
+            text="102.13 CHF (Kurs vom 11.12.2017)"
           />
         </PickerCard>
       </AppModal>
