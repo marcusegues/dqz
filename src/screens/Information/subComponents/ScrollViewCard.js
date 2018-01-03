286e1a6efbe7ef461c74354fcd2cbcef00f7f3b0
Jevgenijs Brencis <jevgen@mail.ru>
2018-01-03 16:37:51 +0200

    --CloseButton component created

---
+++ src/screens/Information/subComponents/ScrollViewCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, ScrollView } from 'react-native';
+import globalStyles from '../../../styles/globalStyles';
+
+const ownStyles = {
+  container: {
+    flex: 1,
+    width: '90%',
+    alignSelf: 'center',
+    marginBottom: 16,
+  },
+  scrollView: {
+    width: '100%',
+    borderColor: '#E0E0E1',
+    backgroundColor: '#fff',
+    borderRadius: 3,
+  },
+};
+
+const ScrollViewCard = ({ children }) => (
+  <View style={ownStyles.container}>
+    <ScrollView
+      contentContainerStyle={[ownStyles.scrollView, globalStyles.boxShadow]}
+    >
+      {children}
+    </ScrollView>
+  </View>
+);
+
+export default ScrollViewCard;
