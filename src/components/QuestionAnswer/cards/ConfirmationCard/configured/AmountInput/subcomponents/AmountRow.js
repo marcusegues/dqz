c75c2247b7fa59ea9248c391f1c177a770248f60
Jevgenijs Brencis <jevgen@mail.ru>
2018-01-04 16:03:16 +0200

    --refactoring of `pickerModal`

--- src/components/QuestionAnswer/cards/ConfirmationCard/configured/AmountInput/subcomponents/AmountRow.js
+++ src/components/QuestionAnswer/cards/ConfirmationCard/configured/AmountInput/subcomponents/AmountRow.js
@@ -10,32 +10,29 @@ import CardRowText from '../../../../subcomponents/CardRowText';
 import { moderateScale } from '../../../../../../../styles/Scaling';
 import { GREY } from '../../../../../../../styles/colors';
 import type { Currency } from '../../../../../../../model/currencies';
+import CardRowSubText from '../../../../subcomponents/CardRowSubText';
 
 type AmountsRowProps = {
   amount: number,
   onDelete: () => void,
   currency: Currency,
-  borderTop?: boolean,
+  // borderTop?: boolean,
 };
 
-const AmountsRow = ({
-  amount,
-  onDelete,
-  currency,
-  borderTop,
-}: AmountsRowProps) => (
-  <Row borderTop={borderTop}>
+const AmountsRow = ({ amount, onDelete, currency }: AmountsRowProps) => (
+  <Row>
     <View style={{ flex: 1 }}>
       <CardRowText text={`${currency} ${amount}`} />
+      <CardRowSubText text={`${currency} ${amount}`} />
     </View>
     <Touchable onPress={onDelete}>
       <MaterialIcons name="cancel" size={moderateScale(28)} color={GREY} />
     </Touchable>
   </Row>
 );
-
-AmountsRow.defaultProps = {
-  borderTop: false,
-};
+//
+// AmountsRow.defaultProps = {
+//   borderTop: false,
+// };
 
 export default AmountsRow;
