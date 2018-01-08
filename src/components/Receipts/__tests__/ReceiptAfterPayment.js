58d5498eb0c43686f90c419cd303b31e8451d97e
Christian Roth <chroth@me.com>
2018-01-08 15:15:30 +0100

    DAZIT-157 Adds tests if receipt renders.

---
+++ src/components/Receipts/__tests__/ReceiptAfterPayment.js
@@ -0,0 +1,16 @@
+/* global describe, test, expect, jest */
+import React from 'react';
+import { shallow } from 'enzyme';
+import ReceiptAfterPayment from '../ReceiptAfterPayment';
+
+jest.mock('react-i18next', () => ({
+  translate: () => Component => props => <Component t={() => ''} {...props} />,
+}));
+
+describe('ReceiptAfterPayment', () => {
+  test('renders according to snapshot', () => {
+    const component = shallow(<ReceiptAfterPayment />).dive();
+    expect(component).toMatchSnapshot();
+    expect(component.find('RedLogo').length).toBe(1);
+  });
+});
