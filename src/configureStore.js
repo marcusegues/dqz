4b7501b1f107357bd2e43b221a21b404f75da587
Christian Roth <chroth@me.com>
2018-01-10 13:19:20 +0100

    Add js doc and missing snapshot.

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,7 +1,7 @@
 // @flow
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import root from './reducers';
+import { root } from './reducers';
 
 export const configureStore = () =>
   createStore(root, applyMiddleware(thunkMiddleware));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -17,34 +17,80 @@ import { calculateVat } from '../model/vatCalculations';
 import { calculateDuty } from '../model/dutyCalculations';
 import type { DutyReport, VatReport } from '../model/types/calculationTypes';
 
-const root = combineReducers({ appState: appApi.appState });
+/**
+ * Combines reducers (currently just one)
+ * @type {Reducer<any>}
+ */
+export const root = combineReducers({ appState: appApi.appState });
 
-export default root;
-
+/**
+ * Gets basket in application state
+ * @param state
+ * @returns {Basket}
+ */
 export const getBasket = (state: { appState: State }): Basket =>
   appApi.getBasket(state.appState);
 
+/**
+ * Gets people in application state
+ * @param state
+ * @returns {People}
+ */
 export const getPeople = (state: { appState: State }): People =>
   appApi.getPeople(state.appState);
 
+/**
+ * Gets amounts in application state
+ * @param state
+ * @returns {Amounts}
+ */
 export const getAmounts = (state: { appState: State }): Amounts =>
   appApi.getAmounts(state.appState);
 
+/**
+ * Gets main categories in application state
+ * @param state
+ * @returns {MainCategories}
+ */
 export const getMainCategories = (state: { appState: State }): MainCategories =>
   appApi.getMainCategories(state.appState);
 
+/**
+ * Gets settings in application state
+ * @param state
+ * @returns {Settings}
+ */
 export const getSettings = (state: { appState: State }): Settings =>
   appApi.getSettings(state.appState);
 
+/**
+ * Gets currencies in application state
+ * @param state
+ * @returns {CurrencyObject}
+ */
 export const getCurrencies = (state: { appState: State }): CurrencyObject =>
   appApi.getCurrenciesObject(state.appState);
 
+/**
+ * Gets currency state in application state
+ * @param state
+ * @returns {boolean}
+ */
 export const getCurrencyState = (state: { appState: State }): boolean =>
   appApi.getCurrencyState(state.appState);
 
+/**
+ * Gets the formatted currency date in application state
+ * @param state
+ * @returns {string}
+ */
 export const getFormattedCurrencyDate = (state: { appState: State }): string =>
   formatDate(appApi.getCurrencyDate(state.appState));
 
+/**
+ * Gets the total vat in application state
+ * @param state
+ */
 export const getTotalVat = (state: { appState: State }): number =>
   calculateVat(
     state.appState.amounts,
@@ -52,18 +98,37 @@ export const getTotalVat = (state: { appState: State }): number =>
     state.appState.currencyObject
   ).get('totalVat', 0);
 
+/**
+ * Gets the total duty in application state
+ * @param state
+ */
 export const getTotalDuty = (state: { appState: State }): number =>
   calculateDuty(state.appState.basket, state.appState.people).get(
     'totalDuty',
     0
   );
 
+/**
+ * Gets the total fees (== vat + duty) in application state
+ * @param state
+ * @returns {number}
+ */
 export const getTotalFees = (state: { appState: State }): number =>
   getTotalVat(state) + getTotalDuty(state);
 
+/**
+ * Gets the duty report in application state
+ * @param state
+ * @returns {DutyReport}
+ */
 export const getDutyReport = (state: { appState: State }): DutyReport =>
   calculateDuty(state.appState.basket, state.appState.people);
 
+/**
+ * Gets the vat report in application state
+ * @param state
+ * @returns {VatReport}
+ */
 export const getVatReport = (state: { appState: State }): VatReport =>
   calculateVat(
     state.appState.amounts,
