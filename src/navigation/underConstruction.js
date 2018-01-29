a85945662f2ae93c232a5ede630386ae848da469
Christian Roth <chroth@me.com>
2018-01-29 12:38:41 +0100

    DAZIT-135 Remove temp implementation in underConstruction.

--- src/navigation/underConstruction.js
+++ src/navigation/underConstruction.js
@@ -3,12 +3,6 @@
 import React from 'react';
 // $FlowFixMe
 import { Text, View } from 'react-native';
-import { RedButton } from '../components/Buttons/RedButton';
-import {
-  storeCurrencyObject,
-  fetchCurrencyObject,
-} from '../asyncStorage/storageApi';
-import { currencyExample } from '../model/currencies';
 
 const style = {
   display: 'flex',
@@ -19,21 +13,5 @@ const style = {
 export const UnderConstruction = () => (
   <View style={style}>
     <Text style={{ fontSize: 64 }}>🚧🛃🚧</Text>
-    <Text>Async Test</Text>
-    <RedButton
-      text="Write"
-      onPress={() => {
-        storeCurrencyObject(currencyExample);
-      }}
-    />
-    <RedButton
-      text="Read"
-      onPress={() => {
-        fetchCurrencyObject()
-          // eslint-disable-next-line no-console
-          .then(v => console.log(v))
-          .catch();
-      }}
-    />
   </View>
 );
