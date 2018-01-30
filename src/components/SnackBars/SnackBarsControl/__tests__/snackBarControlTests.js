3a571278f89298a8ac6661742b509439b551a0d5
Marcus Egues <marcusegues@mbp-sf.local>
2018-01-30 16:52:52 +0100

    Add one more test

--- src/components/SnackBars/SnackBarsControl/__tests__/snackBarControlTests.js
+++ src/components/SnackBars/SnackBarsControl/__tests__/snackBarControlTests.js
@@ -24,5 +24,24 @@ describe('Test snack bar control flow', () => {
 
         expect(updateState(newState).snackBarStates.limitExceeded).toBe('displayed');
     });
+    test('adding value below declaration limit does not show limitExceeded', () => {
+        const newState = {
+            ...initStateEnriched,
+            amounts: addAmount(initStateEnriched.amounts, 'CHF', (MAX_DECLARED_CHF - 1))
+        }
+
+        expect(updateState(newState).snackBarStates.limitExceeded).toBe('hidden');
+    });
+    test('adding multiple values summing to below declaration limit does not show limitExceeded', () => {
+        const amount1 = addAmount(initStateEnriched.amounts, 'CHF', (MAX_DECLARED_CHF - 10));
+        const amount2 = addAmount(amount1, 'CHF', 1);
+        const amount3 = addAmount(amount2, 'EUR', 1);
+        const newState = {
+            ...initStateEnriched,
+            amounts: amount3
+        }
+
+        expect(updateState(newState).snackBarStates.limitExceeded).toBe('hidden');
+    });
 });
 
