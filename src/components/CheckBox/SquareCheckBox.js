453c648c1ca6d821838f1721cf617de64d12d8c5
Christian Roth <chroth@me.com>
2018-02-02 13:33:35 +0100

    DAZIT-211 add functionality to modal.

--- src/components/CheckBox/SquareCheckBox.js
+++ src/components/CheckBox/SquareCheckBox.js
@@ -1,24 +1,34 @@
 // @flow
 import React from 'react';
 // $FlowFixMe
+import Touchable from 'react-native-platform-touchable';
+// $FlowFixMe
 import { View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { moderateScale } from '../../styles/Scaling';
 
-export const SquareCheckBox = ({ checked }: { checked: boolean }) => (
+export const SquareCheckBox = ({
+  checked,
+  onPress,
+}: {
+  checked: boolean,
+  onPress: () => void,
+}) => (
   <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-    {checked ? (
-      <MaterialCommunityIcons
-        name="checkbox-marked"
-        size={moderateScale(25)}
-        color="#4A4A4A"
-      />
-    ) : (
-      <MaterialCommunityIcons
-        name="checkbox-blank-outline"
-        size={moderateScale(25)}
-        color="#4A4A4A"
-      />
-    )}
+    <Touchable onPress={() => onPress()}>
+      {checked ? (
+        <MaterialCommunityIcons
+          name="checkbox-marked"
+          size={moderateScale(25)}
+          color="#4A4A4A"
+        />
+      ) : (
+        <MaterialCommunityIcons
+          name="checkbox-blank-outline"
+          size={moderateScale(25)}
+          color="#4A4A4A"
+        />
+      )}
+    </Touchable>
   </View>
 );
--- src/components/Modals/LegalNoticeModal/LegalNoticeModal.js
+++ src/components/Modals/LegalNoticeModal/LegalNoticeModal.js
@@ -20,71 +20,95 @@ import { scale, verticalScale } from '../../../styles/Scaling';
 import { ModalCloseText } from '../ModalCloseText';
 import { SquareCheckBox } from '../../CheckBox/SquareCheckBox';
 
+type LegalNoticeModalState = {
+  checked: boolean,
+};
 type LegalNoticeModalProps = {
   modalVisible: boolean,
   toggleModalVisible: () => void,
 };
 
-const LegalNoticeModalInner = ({
-  modalVisible,
-  toggleModalVisible,
-}: LegalNoticeModalProps) => (
-  <AppModal
-    modalVisible={modalVisible}
-    animationIn="slideInUp"
-    animationOut="slideOutDown"
-  >
-    <PickerCard
-      style={{
-        width: '100%',
-      }}
-    >
-      <CardHeader text="Wichtiger Hinweis:" />
-
-      <CardHeaderSubText
-        text="Bitte berücksichtigen Sie, dass bei der Benutzung dieser App keine Rückerstattung möglich ist. Deklarierte Waren werden mit dem einheitlichen Mehrwertsteuersatz von 7.7% verzollt. Weitere Informationen finden Sie hier:"
-        style={{ color: '#757575', lineHeight: 18 }}
-      />
-
-      <TouchableWithoutFeedback>
-        <CardHeaderSubText
-          text="ALLGEIME GESCHÄFTBEDINGUNGEN"
-          style={{
-            marginTop: verticalScale(10),
-            marginBottom: verticalScale(20),
-            fontFamily: 'roboto_medium',
-            color: '#757575',
-          }}
-        />
-      </TouchableWithoutFeedback>
-
-      <View
-        style={{
-          marginHorizontal: scale(16),
-          flexDirection: 'row',
-          justifyContent: 'flex-start',
-          alignItems: 'flex-start',
-        }}
+class LegalNoticeModalInner extends React.Component<
+  LegalNoticeModalProps,
+  LegalNoticeModalState
+> {
+  constructor(props: LegalNoticeModalProps) {
+    super(props);
+    this.state = {
+      checked: false,
+    };
+  }
+  render() {
+    const { checked } = this.state;
+    const { modalVisible, toggleModalVisible } = this.props;
+    return (
+      <AppModal
+        modalVisible={modalVisible}
+        animationIn="slideInUp"
+        animationOut="slideOutDown"
       >
-        <SquareCheckBox checked />
-        <CardHeaderSubText
-          style={{ color: '#4a4a4a', paddingRight: scale(16) }}
-          text="Hiermit akzeptiere ich die Allgemeinen Geschäftsbedingungen, die ich gelesen und verstanden habe."
-        />
-      </View>
-
-      <View style={pickerModalStyle.redButtonWrapper}>
-        <RedButton
-          onPress={() => {
-            Keyboard.dismiss();
+        <PickerCard
+          style={{
+            width: '100%',
           }}
-          text="BESTÄTIGEN"
-        />
-      </View>
-    </PickerCard>
-    <ModalCloseText onModalHide={toggleModalVisible} text="Abbrechen" />
-  </AppModal>
-);
+        >
+          <CardHeader text="Wichtiger Hinweis:" />
+
+          <CardHeaderSubText
+            text="Bitte berücksichtigen Sie, dass bei der Benutzung dieser App keine Rückerstattung möglich ist. Deklarierte Waren werden mit dem einheitlichen Mehrwertsteuersatz von 7.7% verzollt. Weitere Informationen finden Sie hier:"
+            style={{
+              color: '#757575',
+              lineHeight: 18,
+            }}
+          />
+
+          <TouchableWithoutFeedback>
+            <CardHeaderSubText
+              text="ALLGEIME GESCHÄFTBEDINGUNGEN"
+              style={{
+                marginTop: verticalScale(10),
+                marginBottom: verticalScale(20),
+                fontFamily: 'roboto_medium',
+                color: '#757575',
+              }}
+            />
+          </TouchableWithoutFeedback>
+
+          <View
+            style={{
+              marginHorizontal: scale(16),
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              alignItems: 'flex-start',
+            }}
+          >
+            <SquareCheckBox
+              checked={checked}
+              onPress={() => console.log('booom')}
+            />
+            <CardHeaderSubText
+              style={{
+                color: '#4a4a4a',
+                paddingRight: scale(16),
+              }}
+              text="Hiermit akzeptiere ich die Allgemeinen Geschäftsbedingungen, die ich gelesen und verstanden habe."
+            />
+          </View>
+
+          <View style={pickerModalStyle.redButtonWrapper}>
+            <RedButton
+              onPress={() => {
+                Keyboard.dismiss();
+              }}
+              text="BESTÄTIGEN"
+            />
+          </View>
+        </PickerCard>
+        <ModalCloseText onModalHide={toggleModalVisible} text="Abbrechen" />
+      </AppModal>
+    );
+  }
+}
 
 export const LegalNoticeModal = (translate(['modal'])(
   LegalNoticeModalInner
