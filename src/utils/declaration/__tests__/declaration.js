3d1bf7f337716ef511b83680cc03cc9730d07fd4
Jevgenijs Brencis <jevgen@mail.ru>
2018-03-05 16:26:46 +0200

    --test passed

--- src/utils/declaration/__tests__/declaration.js
+++ src/utils/declaration/__tests__/declaration.js
@@ -25,9 +25,7 @@ const amountsWithNormalAmount = addAmount(initAmounts, 'CHF', 5000);
 
 const amountsWithLargeAmount = addLargeAmount(initAmounts, 'CHF', 500);
 
-const nonEmptyReceiptEntryTime = DateTime.local(2018, 1, 1, 8, 30)({
-  keepLocalTime: true,
-}).toString();
+const nonEmptyReceiptEntryTime = DateTime.local(2018, 1, 1, 8, 30).toString();
 
 describe('isInitBasket', () => {
   test('init values of declaration returns true', () => {
