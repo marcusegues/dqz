8ea340004d48489bd472701d714d6c14980cf210
Marcus Egues <marcusegues@mbp-sf.local>
2018-03-14 11:08:52 +0100

    Add swiping to delete in quantity list modal

--- src/components/Modals/GoodQuantityListModal/subcomponents/QuantityRow.js
+++ src/components/Modals/GoodQuantityListModal/subcomponents/QuantityRow.js
@@ -1,21 +1,17 @@
 // @flow
 import React from 'react';
 // $FlowFixMe
-import Touchable from 'react-native-platform-touchable';
-// $FlowFixMe
-import { View } from 'react-native';
+import { View, TouchableWithoutFeedback } from 'react-native';
 import { translate } from 'react-i18next';
 import type { ComponentType } from 'react';
-import { MaterialIcons } from '@expo/vector-icons';
 import { CardRowText } from '../../../QuestionAnswer/Cards/subcomponents/CardRowText';
-import { moderateScale } from '../../../../styles/Scaling';
-import { GREY } from '../../../../styles/colors';
 import { CategoriesInfo } from '../../../../model/constants';
 import { Row } from '../../../Rows/Row';
 import type { Category } from '../../../../model/types/basketPeopleAmountsTypes';
 import { rowStyles } from '../../../Rows/styles/rowStyles';
 import { CardRowSubText } from '../../../QuestionAnswer/Cards/subcomponents/CardRowSubText';
 import type { TFunction } from '../../../../types/generalTypes';
+import { SwipeToDelete } from '../../../General Components/SwipeableContent/configured/SwipeToDelete';
 
 type QuantityRowInnerProps = {
   quantity: number,
@@ -34,20 +30,21 @@ const QuantityRowInner = ({
   borderTop = false,
 }: QuantityRowInnerProps & { t: TFunction }) => (
   <Row borderTop={borderTop}>
-    <View style={rowStyles.rowContent}>
-      <View style={{ flex: 1 }}>
-        <CardRowText
-          text={`${quantity.toFixed(3)} ${t(
-            `units:${CategoriesInfo.getIn([category, 'unit'], '')}`,
-            { count: quantity }
-          )}`}
-        />
-        <CardRowSubText text={t('recordedOn', { value: date })} />
-      </View>
-      <Touchable onPress={onDelete}>
-        <MaterialIcons name="cancel" size={moderateScale(28)} color={GREY} />
-      </Touchable>
-    </View>
+    <SwipeToDelete onPressDelete={onDelete}>
+      <TouchableWithoutFeedback>
+        <View style={rowStyles.rowContent}>
+          <View style={{ flex: 1 }}>
+            <CardRowText
+              text={`${quantity.toFixed(3)} ${t(
+                `units:${CategoriesInfo.getIn([category, 'unit'], '')}`,
+                { count: quantity }
+              )}`}
+            />
+            <CardRowSubText text={t('recordedOn', { value: date })} />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </SwipeToDelete>
   </Row>
 );
 
