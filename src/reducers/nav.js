1de1fc13c352368087651f62b9bf8bbd2d2180cb
Marcus Egues <marcusegues@mbp-sf.local>
2018-03-14 10:09:52 +0100

    Update stale test for initial screen

--- src/reducers/nav.js
+++ src/reducers/nav.js
@@ -1,8 +1,25 @@
 import { NavigationActions } from 'react-navigation';
 import { RootStackNavigator } from '../navigation/RootNavigation';
 
+/*
+Navigation state has structure
+{
+  index: // identifies which route in the routes array is active
+  routes:[
+    {
+      // Each route needs a name, which routers will use to associate each route
+      // with a react component
+      routeName: 'MyRouteName',
+      key: 'myroute-123'  // Unique id
+      ...customRouteData  // Additional data
+    },
+    ...moreRoutes,
+  ]
+}
+*/
+
 export const initialNavState = RootStackNavigator.router.getStateForAction(
-  RootStackNavigator.router.getActionForPathAndParams('MainMenu')
+  RootStackNavigator.router.getActionForPathAndParams('OnBoarding')
 );
 
 export const nav = (state = initialNavState, action) => {
