b686f7b38874c2561297e149699d2b59e9a0cb64
Marcus Egues <marcusegues@mbp-sf.local>
2018-03-19 10:49:57 +0100

    Add x

--- src/components/Modals/GoodQuantityListModal/subcomponents/QuantityRow.js
+++ src/components/Modals/GoodQuantityListModal/subcomponents/QuantityRow.js
@@ -2,6 +2,9 @@
 import React from 'react';
 // $FlowFixMe
 import { View, TouchableWithoutFeedback } from 'react-native';
+// $FlowFixMe
+import Touchable from 'react-native-platform-touchable';
+import { MaterialIcons } from '@expo/vector-icons';
 import { translate } from 'react-i18next';
 import type { ComponentType } from 'react';
 import { CardRowText } from '../../../QuestionAnswer/Cards/subcomponents/CardRowText';
@@ -12,7 +15,9 @@ import { rowStyles } from '../../../Rows/styles/rowStyles';
 import { CardRowSubText } from '../../../QuestionAnswer/Cards/subcomponents/CardRowSubText';
 import type { TFunction } from '../../../../types/generalTypes';
 import { SwipeToDelete } from '../../../General Components/SwipeableContent/configured/SwipeToDelete';
-import {formatQuantity} from "../../../../utils/declaration/declaration";
+import { formatQuantity } from '../../../../utils/declaration/declaration';
+import { moderateScale } from '../../../../styles/Scaling';
+import { GREY } from '../../../../styles/colors';
 
 type QuantityRowInnerProps = {
   quantity: number,
@@ -36,12 +41,22 @@ const QuantityRowInner = ({
         <View style={rowStyles.rowContent}>
           <View style={{ flex: 1 }}>
             <CardRowText
-              text={`${formatQuantity(category, quantity)} ${t(`units:${CategoriesInfo.getIn([category, 'unit'], '')}`, {
-                count: quantity,
-              })}`}
+              text={`${formatQuantity(category, quantity)} ${t(
+                `units:${CategoriesInfo.getIn([category, 'unit'], '')}`,
+                {
+                  count: quantity,
+                }
+              )}`}
             />
             <CardRowSubText text={t('recordedOn', { value: date })} />
           </View>
+          <Touchable onPress={onDelete}>
+            <MaterialIcons
+              name="cancel"
+              size={moderateScale(28)}
+              color={GREY}
+            />
+          </Touchable>
         </View>
       </TouchableWithoutFeedback>
     </SwipeToDelete>
