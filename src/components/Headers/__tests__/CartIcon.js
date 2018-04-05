965fbe976076c5126594258dbbf819a8d03c8c14
Christian Roth <chroth@me.com>
2018-04-05 10:35:03 +0200

    Fix tests when clicking a button in CartIcon.

--- src/components/Headers/__tests__/CartIcon.js
+++ src/components/Headers/__tests__/CartIcon.js
@@ -1,3 +1,4 @@
+// @flow
 import renderer from 'react-test-renderer';
 import React from 'react';
 import { shallow } from 'enzyme';
@@ -5,28 +6,36 @@ import { shallow } from 'enzyme';
 import Touchable from 'react-native-platform-touchable';
 import { CartIcon } from '../subcomponents/CartIcon';
 
-test('CartIcon component renders correctly', () => {
-  const tree = renderer.create(<CartIcon onPress={() => {}} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
-//
-// test('onPress', () => {
-//   let i = 0;
-//   const onPress = () => {
-//     i += 1;
-//   };
-//   const wrapperPress = shallow(<CartIcon onPress={onPress} />);
-//
-//   expect(wrapperPress.prop('onPress')).toBe(onPress); // uses the right handler
-//   expect(i).toBe(0);
-//   wrapperPress.simulate('press');
-//   expect(i).toBe(1);
-// });
-//
-//
-test('we can click it', () => {
-  const clickFn = jest.fn();
-  const component = shallow(<CartIcon onPress={clickFn} />);
-  component.find(Touchable).simulate('press');
-  expect(clickFn).toBeCalled();
+describe('Cart Icon TestSuite', () => {
+  test('CartIcon component renders correctly', () => {
+    // $FlowFixMe
+    const tree = renderer.create(<CartIcon navigation={undefined} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  //
+  // test('onPress', () => {
+  //   let i = 0;
+  //   const onPress = () => {
+  //     i += 1;
+  //   };
+  //   const wrapperPress = shallow(<CartIcon onPress={onPress} />);
+  //
+  //   expect(wrapperPress.prop('onPress')).toBe(onPress); // uses the right handler
+  //   expect(i).toBe(0);
+  //   wrapperPress.simulate('press');
+  //   expect(i).toBe(1);
+  // });
+  //
+  //
+  test('we can click it', () => {
+    const clickFn = jest.fn();
+    // $FlowFixMe
+    const component = shallow(<CartIcon navigation={{ dispatch: clickFn }} />);
+    component.find(Touchable).simulate('press');
+    expect(clickFn).toBeCalled();
+    expect(clickFn).toBeCalledWith({
+      type: 'NAVIGATE',
+      screen: 'QuestionAnswer',
+    });
+  });
 });
