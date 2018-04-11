1ebc857f0107165153770e071cf33392c8b885c7
Jevgenijs Brencis <jevgen@mail.ru>
2018-04-11 14:06:56 +0300

    --tests for `FormattedText` created

---
+++ src/screens/Information/subComponents/__tests__/FormattedText.js
@@ -0,0 +1,34 @@
+import React from 'react';
+// $FlowFixMe
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import { FormattedText } from '../FormattedText';
+
+describe('FormattedText', () => {
+  test('FormattedText renders according to snapshot', () => {
+    const component = renderer
+      .create(<FormattedText text="MockCollapsedCardText" />)
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+  test('FormattedText component renders correctly if color prop is set', () => {
+    const tree = renderer
+      .create(
+        <FormattedText text="MockCollapsedCardText" style={{ color: 'blue' }} />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('FormattedText component renders correctly if bold prop is passed', () => {
+    const tree = renderer
+      .create(<FormattedText text="FormattedText test" bold />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('renders as expected', () => {
+    const wrapper = shallow(<FormattedText style={{ fontSize: 20 }} bold />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.setProps({ bold: false });
+    expect(wrapper).toMatchSnapshot();
+  });
+});
