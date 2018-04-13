2d0b29801e6d6d60ee129f9c648a17c42fb5216f
Yuri Kravcenko <yuri.kravcenko@gmail.com>
2018-04-13 11:56:32 +0300

    LegalNoticeInfo.test implementation

--- src/screens/LegalNoticeInfo/__tests__/LegalNoticeInfo.test.js
+++ src/screens/LegalNoticeInfo/__tests__/LegalNoticeInfo.test.js
@@ -1,26 +1,18 @@
-// import React from 'react';
-// import renderer from 'react-test-renderer';
-// import configureStore from 'redux-mock-store';
-// import { LegalNoticeInfo } from '../LegalNoticeInfo';
-//
-// const middlewares = []; // you can mock any middlewares here if necessary
-// const mockStore = configureStore(middlewares);
-//
-// const initialState = {
-//   selected: 'usage',
-// };
-//
-// jest.mock('react-i18next', () => ({
-//   translate: () => Component => props => <Component t={() => ''} {...props} />,
-// }));
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { LegalNoticeInfo } from '../LegalNoticeInfo';
+
+jest.mock('react-i18next', () => ({
+  translate: () => Component => props => <Component t={() => ''} {...props} />,
+  Trans: () => Component => props => <Component t={() => ''} {...props} />,
+}));
 
 describe('LegalNoticeInfo snapshot test', () => {
-  test('Renders empty AllReceipts component to snapshot', () => {
-    // const tree = await renderer.create(
-    //   <LegalNoticeInfo i18n={{ language: 'de' }} />
-    // );
-    // const json = await tree.toJSON();
-    // expect(json).toMatchSnapshot();
-    expect(1 + 1).toBe(2);
+  test('Renders empty AllReceipts component to snapshot', async () => {
+    const tree = await renderer.create(
+      <LegalNoticeInfo i18n={{ language: 'de' }} />
+    );
+    const json = await tree.toJSON();
+    expect(json).toMatchSnapshot();
   });
 });
